9574051a287953d1d3ffe86ea26016c9beb263bf
GleidsonLM <gleidsonlm@gmail.com>
2022-10-28 18:47:07 -0300

    completed checksExistsUserAccount , checksCreateTodosUserAvailability and checksTodoExists

--- src/index.js
+++ src/index.js
@@ -9,16 +9,84 @@ app.use(cors());
 
 const users = [];
 
+/* 
+  User for testing purposes.
+*/
+users.push({
+  id: uuidv4(),
+  name: "Gleidson",
+  username: "gleidsonlm",
+  pro: false,
+  todos: []
+},{
+  id: uuidv4(),
+  name: "Gleidson",
+  username: "gmedeiros",
+  pro: true,
+  todos: []
+});
+
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  // find user by username in header and pass it to request.user
+  const user = request.headers;
+  if (users.some(e => e.username === username)){
+    request.user = user;
+    return next();
+  } else {
+  // find a non existing user by username in header
+    return response.status(404).json({ error: 'User not found' });
+  }
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  // let user create a new todo when is in free plan and have less than ten todos
+  if (!request.user.pro) {
+    if (request.user.todos.length < 10) {
+      return next();
+      // Don't let user create a new todo when is not Pro and already have ten todos
+      } else {
+        return response.status(403).json({ error: "You have "+ request.user.todos.length + "todo's created. /n You could delete other completed or go Pro and support us." })
+      }
+  } else if (request.user.pro) {
+  //let user create infinite new todos when is in Pro plan
+  return next();
+  } else {
+    return response.status(500).json({ error: "We're unable to check your subscripion, please contact our client service team." })
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  // put user and todo in request when both exits
+  const user = request.headers;
+  const todo = request.params;
+  
+  /* todo: Probably better done with switch, refactor later. */
+  // don't put user and todo in request when user does not exists
+  if (users.find(e => e.username === username)){
+    request.user = user;
+    
+    // don't put user and todo in request when todo id is not uuid
+    if (validate(todo)){ //imported from uuid library.
+      
+      // put user and todo in request when both exits
+      if (user.todo.find(e => e.id === todo.id)) {
+        todo = request.todo
+        return next();
+
+      // don't put user and todo in request when todo does not exists
+      } else {
+        return response.status(404).json({ error: "Todo not found" });
+      } 
+
+    // don't put user and todo in request when todo is not uuid
+    } else {
+      return response.status(404).json({ error: 'Todo is not UUID' });
+    }
+
+  } else {
+  // find a non existing user by username in header
+    return response.status(404).json({ error: 'User not found' });
+  }
 }
 
 function findUserById(request, response, next) {
